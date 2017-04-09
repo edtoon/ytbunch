6d897c53a2213473f54a97008254c2025fad8999
Ed Toon <edtoon@edtoon.com>
2017-04-09 04:00:35 -0700

    V2 - use a complex filter to split the screen

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-What it does:
+Current functionality:
 
   * Version 1: Command that takes N YouTube video IDs, caches them locally
 
   * Version 2: Creates a composite video split horizontally then vertically N times
 
+Planned functionality:
+
   * Version 3: Composite starts with the longest video playing full screen,
     then splits out to include the next video in the timeline.
 
   * Version 4: Always play the audio from the most recent video.
+
+Known issues:
+
+  * The scaling does not preserve aspect ratio properly.
 
 How to display all of the fights between [Peter Griffin](https://en.wikipedia.org/wiki/Peter_Griffin) and [Ernie the Giant Chicken](http://familyguy.wikia.com/wiki/Ernie_The_Giant_Chicken)
 
@@ -28,3 +34,5 @@ Videos in order are -
   * From the video game 'The Quest for Stuff'
   * From the episode 'Internal Affairs'
   * From the episode 'The Simpsons Guy' - Peter vs Homer
+
+Want to add a little of fun by having an odd number of videos? Try tacking on 2Z4m4lnjxkY to the end.
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import uuidV4 from 'uuid/v4'
+import * as Ffmpeg from 'fluent-ffmpeg'
+
+import * as config from './src/config'
 import * as db from './src/db'
 import * as downloader from './src/downloader'
 import * as util from './src/util'
@@ -30,14 +34,15 @@ const main = async () => {
       }
 
       if (!videoInfo) {
-        console.error('No duration / dimension info for uuid: ' + uuid + ', skipping')
+        console.error('No duration / dimension info for uuid: ' + uuid)
+        process.exit(1)
       } else {
         let {width, height, duration} = videoInfo
         let length = util.durationToSeconds(duration)
         let widthInt = parseInt(width, 10)
         let heightInt = parseInt(height, 10)
 
-        videos.push({ uuid, url, length })
+        videos.push({ uuid, url, length, width: widthInt, height: heightInt })
 
         if (length > longestLength) {
           longestLength = length
@@ -63,6 +68,84 @@ const main = async () => {
     console.log('Largest width: ' + largestWidth)
     console.log('Largest height: ' + largestHeight)
     console.log('Videos', videos)
+
+    const outputFile = __dirname + '/' + uuidV4() + '.mp4'
+    const ffmpeg = Ffmpeg.default({
+      logger: {
+        debug: (msg) => console.log('DEBUG: ' + msg),
+        info: (msg) => console.log('INFO: ' + msg),
+        warn: (msg) => console.log('WARN: ' + msg),
+        error: (msg) => console.error('ERROR: ' + msg)
+      }
+    })
+
+    ffmpeg.on('error', error => {
+      console.error('Ffmpeg error', error)
+      process.exit(1)
+    })
+
+    ffmpeg.on('progress', (i) => {
+      console.log('Ffmpeg progress: ' + Math.floor(i.percent) + '%');
+    })
+
+    ffmpeg.on('end', error => {
+      console.log('Ffmpeg done')
+    })
+
+    const rowCount = Math.floor(videos.length / 2) + ((videos.length % 2) ? 1 : 0)
+    const rowWidth = largestWidth
+    const columnWidth = Math.floor(rowWidth / 2)
+    const rowHeight = Math.floor(largestHeight / rowCount)
+    console.log('Rows: ' + rowCount + ', width: ' + rowWidth + ', height: ' + rowHeight + ', column height: ' + columnWidth)
+    let currentRow = 0
+    let isLeftFrame = false
+    const filterInputs = []
+    const filterOutputs = []
+
+    for (let i = 0; i < videos.length; i++) {
+      isLeftFrame = !isLeftFrame
+
+      if (isLeftFrame) {
+        currentRow++
+      }
+
+      const currentVideo = videos[i]
+      const previousVideo = (i === 0 ? null : videos[i - 1])
+      const nextVideo = (i === (videos.length + 1)) ? null : videos[i + 1]
+      const videoDir = config.getVideoDir(currentVideo.url, currentVideo.uuid)
+      const videoFile = videoDir + '/' + config.getVideoFileName(currentVideo.url, currentVideo.uuid)
+      const streamName = ('row' + currentRow + (isLeftFrame ? 'left' : 'right'))
+      const hackyWidth = ((!isLeftFrame || nextVideo) ? Math.floor(rowWidth / 2) : rowWidth)
+      const xOffset = (isLeftFrame ? null : ('x=' + hackyWidth))
+      const yOffset = (currentRow === 1 ? null : ('y=' + Math.floor((currentRow - 1) * rowHeight)))
+      const overlayCoords = (
+        (!xOffset && !yOffset) ?
+          ''
+          :
+          '=' +
+            (xOffset ? xOffset : '') +
+            (yOffset ? ((xOffset ? ':' : '') + yOffset) : '')
+      )
+
+      console.log('Index (' + i + ') is ' + (isLeftFrame ? 'left' : 'right') + ' frame of row: ' + currentRow + ' - ' + currentVideo.url)
+
+      filterInputs.push('[' + i + ':v] setpts=PTS-STARTPTS, scale=' + ((nextVideo || !isLeftFrame) ? columnWidth : rowWidth) + 'x' + rowHeight + ' [' + streamName + ']')
+      filterOutputs.push('[' + (i === 0 ? 'base' : ('tmp' + i)) + '][' + streamName + '] overlay' + overlayCoords + (nextVideo ? (' [tmp' + (i+1) + ']') : ''))
+      ffmpeg.input(videoFile)
+    }
+
+    const filters = [
+      'nullsrc=size=' + largestWidth + 'x' + largestHeight + ' [base]',
+      ...filterInputs,
+      ...filterOutputs
+    ]
+
+    console.log('Complex filters', filters)
+    ffmpeg.complexFilter(filters)
+    ffmpeg.audioCodec('aac')
+    ffmpeg.videoCodec('libx264')
+    ffmpeg.outputOptions(['-preset ultrafast', '-shortest'])
+    ffmpeg.output(outputFile).run()
   }
 }
 
